0dcf890a64254011a464719b0427da38d86e05cf
artist <artist@yandex-team.ru>
2018-04-02 20:13:00 +0300

    auth2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hello: "world" });
-});
+passport.use(
+    new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, (accsessToken) => {
+      console.log('accsessToken', accsessToken);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
 
 app.listen(PORT);
